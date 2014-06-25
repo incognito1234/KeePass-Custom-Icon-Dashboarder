3ded85720bb14f87666506c0155b54fd758522b4
Jareth Lomson <jareth.lomson@gmail.com>
2014-06-25 16:12:46 +0200

    Update script to create plgx

--- createPlgx.cmd
+++ createPlgx.cmd
@@ -1,11 +1,36 @@
-﻿@echo on
+﻿@echo off
 
-set PATH_KEEPASS=C:\
+rem Uncomment this line if PATH_KEEPASS is not an environment variable
+rem set PATH_KEEPASS=C:\
 
-set PATH_PLUGIN=%~dp0
-rem set PATH_PLUGIN=%PATH_PLUGIN%CustomIconDashboarder
+set SRC_PATH_PLUGIN=%~dp0
 
-%PATH_KEEPASS%\keepass.exe --plgx-create %PATH_PLUGIN% ^
+rem WARNING
+rem this folder is removed at start and end of this command file
+set TMP_PATH_PLUGIN=%TEMP%\CustomIconDashboarder
+
+rmdir /s /q %TMP_PATH_PLUGIN%
+mkdir %TMP_PATH_PLUGIN%
+
+xcopy %SRC_PATH_PLUGIN%*.* %TMP_PATH_PLUGIN%
+del %TMP_PATH_PLUGIN%\%~nx0
+
+mkdir %TMP_PATH_PLUGIN%\Properties
+xcopy /S %SRC_PATH_PLUGIN%Properties %TMP_PATH_PLUGIN%\Properties
+
+mkdir %TMP_PATH_PLUGIN%\UI
+xcopy /S %SRC_PATH_PLUGIN%UI %TMP_PATH_PLUGIN%\UI
+
+%PATH_KEEPASS%\keepass.exe --plgx-create %TMP_PATH_PLUGIN% ^
    --plgx-prereq-kp:2.0 ^
    --plgx-prereq-net:2.0
+
+copy %TEMP%\CustomIconDashboarder.plgx %~dp0
+
+echo Remove Temp files
+
+rmdir /s /q %TMP_PATH_PLUGIN%
+mkdir %TMP_PATH_PLUGIN%
+rm %TEMP%\CustomIconDashboarder.plgx
+   
 
