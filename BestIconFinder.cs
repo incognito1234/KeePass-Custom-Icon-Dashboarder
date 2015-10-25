649758ecaa9553cd93f1976a94cb297cf28a0c79
Jareth Lomson <jareth.lomson@gmail.com>
2015-10-25 15:44:28 +0100

    Add extra memento comment to flag unsafeheaderparsing
    Favicon of some site can not be retrieved without this flag set to true.
    Seems to decrease security so it is not enabled in the current release.

--- BestIconFinder.cs
+++ BestIconFinder.cs
@@ -469,6 +469,40 @@ namespace CustomIconDashboarderPlugin
 				this.BestImage.Dispose();
 			}
 		}
+	
+//       // Set "UnsafeHeaderParsing" flags. Usefull to retrieve information from several sites
+//       // try www dot geni dot com    
+//       // to be used with "using System.Reflection;" in header
+//       public static bool SetAllowUnsafeHeaderParsing(bool value)
+//		{
+//		  //Get the assembly that contains the internal class
+//		  Assembly aNetAssembly = Assembly.GetAssembly(typeof(System.Net.Configuration.SettingsSection));
+//		  if (aNetAssembly != null)
+//		  {
+//		    //Use the assembly in order to get the internal type for the internal class
+//		    Type aSettingsType = aNetAssembly.GetType("System.Net.Configuration.SettingsSectionInternal");
+//		    if (aSettingsType != null)
+//		    {
+//		      //Use the internal static property to get an instance of the internal settings class.
+//		      //If the static instance isn't created allready the property will create it for us.
+//		      object anInstance = aSettingsType.InvokeMember("Section",
+//		        BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.NonPublic, null, null, new object[] { });
+//		
+//		      if (anInstance != null)
+//		      {
+//		        //Locate the private bool field that tells the framework is unsafe header parsing should be allowed or not
+//		        FieldInfo aUseUnsafeHeaderParsing = aSettingsType.GetField("useUnsafeHeaderParsing", BindingFlags.NonPublic | BindingFlags.Instance);
+//		        if (aUseUnsafeHeaderParsing != null)
+//		        {
+//		          aUseUnsafeHeaderParsing.SetValue(anInstance, value);
+//		          return true;
+//		        }
+//		      }
+//		    }
+//		  }
+//		  return false;
+//		} 
+//
 	}
 	
 	public sealed class FinderResult {
@@ -484,6 +518,6 @@ namespace CustomIconDashboarderPlugin
 			ResultCode = code;
 		}
 	}
-
-        
+	
+	
 }
