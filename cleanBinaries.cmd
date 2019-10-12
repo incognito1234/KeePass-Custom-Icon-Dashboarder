37b8f50f4f4c7f1b7555ac73df22f542a2faf589
Jareth Lomson <jareth.lomson@gmail.com>
2019-10-12 18:29:26 +0200

    Update plgx creation script

--- cleanBinaries.cmd
+++ cleanBinaries.cmd
@@ -4,3 +4,8 @@ rmdir /Q /S %~dp0\obj
 rmdir /Q /S %~dp0\bin
 mkdir obj
 mkdir bin
+
+rmdir /Q /S %~dp0\LomsonLib\obj
+rmdir /Q /S %~dp0\LomsonLib\bin
+mkdir LomsonLib\obj
+mkdir LomsonLib\bin
--- createPlgx.cmd
+++ createPlgx.cmd
@@ -1,7 +1,7 @@
-﻿@echo off
+﻿echo off
 
 rem Uncomment this line if PATH_KEEPASS is not an environment variable
-set PATH_KEEPASS=C:\workspace\KeePass\KeePass-2.30
+set PATH_KEEPASS=C:\Temp\KeePass-2.43
 
 set SRC_PATH_PLUGIN=%~dp0
 
@@ -13,6 +13,9 @@ rmdir /s /q %TMP_PATH_PLUGIN%
 mkdir %TMP_PATH_PLUGIN%
 
 xcopy %SRC_PATH_PLUGIN%*.* %TMP_PATH_PLUGIN%
+del %TMP_PATH_PLUGIN%\CustomIconDashboarder.csproj
+copy %TMP_PATH_PLUGIN%\CustomIconDashboarder_plgx.csproj %TMP_PATH_PLUGIN%\CustomIconDashboarder.csproj
+del %TMP_PATH_PLUGIN%\CustomIconDashboarder_plgx.csproj
 del %TMP_PATH_PLUGIN%\%~nx0
 
 mkdir %TMP_PATH_PLUGIN%\HtmlAgilityPack
@@ -21,21 +24,17 @@ xcopy /S %SRC_PATH_PLUGIN%HtmlAgilityPack %TMP_PATH_PLUGIN%\HtmlAgilityPack
 mkdir %TMP_PATH_PLUGIN%\Properties
 xcopy /S %SRC_PATH_PLUGIN%Properties %TMP_PATH_PLUGIN%\Properties
 
-mkdir %TMP_PATH_PLUGIN%\Lib
-xcopy /S %SRC_PATH_PLUGIN%Lib %TMP_PATH_PLUGIN%\Lib
+mkdir %TMP_PATH_PLUGIN%\LomsonLib
+xcopy /S %SRC_PATH_PLUGIN%LomsonLib %TMP_PATH_PLUGIN%\LomsonLib
+del %TMP_PATH_PLUGIN%\LomsonLib\LomsonLib.csproj
+del %TMP_PATH_PLUGIN%\LomsonLib\Properties\AssemblyInfo.cs
 
 %PATH_KEEPASS%\keepass.exe --plgx-create %TMP_PATH_PLUGIN% ^
    --plgx-prereq-kp:2.0 ^
-   --plgx-prereq-net:2.0
+   --plgx-prereq-net:4.5
 
 copy %TEMP%\CustomIconDashboarder.plgx %~dp0
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.25\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.26\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.27\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.28\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.29\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.30\Plugins
-copy %~dp0\CustomIconDashboarder.plgx C:\workspace\KeePass\KeePass-2.31\Plugins
+copy %~dp0\CustomIconDashboarder.plgx C:\temp\KeePass-2.43\Plugins
 
 echo Remove Temp files
 
